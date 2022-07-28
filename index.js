ca4ee409892ad3700a89a9b70f34911a037e34d7
Maria Coitinho <brasildu@gmail.com>
2022-07-28 13:58:39 -0500

    commit tree b43db5cfb56d55892a3812af843bdd1063a50241

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+// Write a function that takes two arguments: an array of numbers and a number as a target. The function should return true if the number passed as an argument is equal to the sum of any pair of numbers in the array. And it should return false otherwise (if any pair of numbers in the array is not equal to the target number passed as an argument)
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1
+  let result = false;
+  // n
+  for (const num of array) {
+  // get index of current num
+  // n
+    let indexOfNum = array.indexOf(num)
+  // get all numbers from the right of num
+  // n
+    let rightOfNum = array.slice(array.indexOf(num) + 1)
+  // get all numbers from the left of num
+  // n
+    let leftOfNum = array.slice(0, indexOfNum)
+  // create array of all array numbers but not the current num
+  // n
+    let restOfArray = rightOfNum.concat(leftOfNum);
+  // make current num iterate with each number of restOfArray to find the target sum
+  // n
+    result = restOfArray.find(n => {
+      return n + num === target
+    })
+  // 1
+    if (typeof(result) === "number") {
+      break;
+  }}
+  return typeof(result) === "number";
 }
 
+hasTargetSum([19, 22, 4, 6, 30], 25)
+
+
+
+
+
 /* 
-  Write the Big O time complexity of your function here
+   O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+
 */
 
 /*
